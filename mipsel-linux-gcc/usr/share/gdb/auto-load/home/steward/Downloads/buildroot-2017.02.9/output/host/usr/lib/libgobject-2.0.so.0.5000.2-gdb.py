540c22ffbece99be104298721de2ba32cc599f04
steward-fu <g9313716@yuntech.edu.tw>
2018-02-12 00:30:26 +0800

    20180212

---
+++ mipsel-linux-gcc/usr/share/gdb/auto-load/home/steward/Downloads/buildroot-2017.02.9/output/host/usr/lib/libgobject-2.0.so.0.5000.2-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/home/steward/Downloads/buildroot-2017.02.9/output/host/usr/share/glib-2.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from gobject_gdb import register
+register (gdb.current_objfile ())
